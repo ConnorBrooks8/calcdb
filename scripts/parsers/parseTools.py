84379c5d2b548cb28465df74585e2a4063d982cf
ConnorBrooks8 <connorbrooks8@gmail.com>
2017-06-09 14:29:14 -0400

    added .travis.yml

--- scripts/parsers/parseTools.py
+++ scripts/parsers/parseTools.py
@@ -1,21 +1,23 @@
 import re
-import numpy
 
-def parseFlags(string,startFlag,endFlag,reFlags=re.S):
+
+def parseFlags(string, startFlag, endFlag, reFlags=re.S):
     Pattern = r"{}(.*?){}".format(startFlag, endFlag)
-    return re.search(Pattern,string,flags=reFlags).group(1)
+    return re.search(Pattern, string, flags=reFlags).group(1)
+
 
 def cleanItem(string):
     string = string.strip()
     intpattern = r'^-?\d+$'
     floatpattern = r'^(-?\d+(\.\d+)?).(\d\d)$'
-    if re.match(intpattern,string):
+    if re.match(intpattern, string):
         return int(string)
-    elif re.match(floatpattern,string):
-        match = re.match(floatpattern,string)
+    elif re.match(floatpattern, string):
+        match = re.match(floatpattern, string)
         return float(match.group(1))*10**(int(match.group(3)))
     else:
         return string
+
 
 def cleanList(string):
     stringList = string.split()
@@ -24,59 +26,60 @@ def cleanList(string):
         cleanList.append(cleanItem(item))
     return cleanList
 
+
 def parseArray(string):
     lines = string.split("\n")
-    #remove empty lines
-    for idx,line in enumerate(lines):
-        if line == "" or re.match("^\s*$",line):
+#   remove empty lines
+    for idx, line in enumerate(lines):
+        if line == "" or re.match("^\s*$", line):
             del lines[idx]
 
     dArray = {}
-    finalArray=[]
+    finalArray = []
 
-    for idx,line in enumerate(lines):
-        whitespace = re.match("\s*(?!\s)",line).group()
+    for idx, line in enumerate(lines):
+        whitespace = re.match("\s*(?!\s)", line).group()
         if idx == 0:
             titleWhitespace = whitespace
             ArrayIndex = cleanList(line)
-            ArrayItemNumber = len(line.split())
         elif whitespace == titleWhitespace:
             ArrayIndex = line.split()
-            ArrayItemNumber = len(line.split())
         else:
             line = cleanList(line)
-            for idx,item in enumerate(reversed(ArrayIndex)):
+            for idx, item in enumerate(reversed(ArrayIndex)):
                 if item not in dArray:
-                    dArray[item]=[]
+                    dArray[item] = []
                 dArray[item].append(line[-(idx+1)])
-    
-    keylist=[]
+
+    keylist = []
 
     for key in dArray.keys():
-        try: 
+        try:
             keylist.append(int(key))
-        except:return dArray
+        except:
+            return dArray
 
     for key in sorted(keylist):
         finalArray.append(dArray[str(key)])
-    
+
     return finalArray
 
-def equivLine(string,name):
+
+def equivLine(string, name):
     pattern = r"({}.*?)(\d(.\d*)?).*\n".format(name)
-    return re.search(pattern,string).group(2)
+    return re.search(pattern, string).group(2)
+
 
 def multiEquivLine(string):
     Dict = {}
-    List=string.split()
+    List = string.split()
     keyList = []
     valueList = []
     itemmarker = 0
-    j=0
-    ###Fixes edge case where equal sign is not at end of string due to negative number
-    newList=[]
+#   Fixes case where equal sign is not at end of string due to negative number
+    newList = []
     for i in range(len(List)):
-        if re.search("=-",List[i]):
+        if re.search("=-", List[i]):
             tempstring = List[i].split('=')
             newList.append(tempstring[0]+'=')
             newList.append(tempstring[1])
@@ -97,30 +100,33 @@ def multiEquivLine(string):
             keyList.append("".join(tempList))
             valueList.append(List[i+1])
             i = i+2
-    for idx,key in enumerate(keyList):
+    for idx, key in enumerate(keyList):
         Dict[key] = valueList[idx]
     return Dict
 
+
 def parseTable(string):
-    subDict={}
-    string = re.split('[-]+\n',string)[-2]
+    subDict = {}
+    string = re.split('[-]+\n', string)[-2]
     rows = string.split('\n')[0:-1]
-    titles = ['Center_Number','Atomic_Number','Atomic_Type','X','Y','Z']
-    for ridx,row in enumerate(rows):
+    titles = ['Center_Number', 'Atomic_Number', 'Atomic_Type', 'X', 'Y', 'Z']
+    for ridx, row in enumerate(rows):
         items = row.split()
-        subDict['Row{}'.format(ridx)]={}
-        for tidx,title in enumerate(titles):
-            subDict['Row{}'.format(ridx)][title]=items[tidx]
+        subDict['Row{}'.format(ridx)] = {}
+        for tidx, title in enumerate(titles):
+            subDict['Row{}'.format(ridx)][title] = items[tidx]
     return subDict
 
 
 def raw(Input):
     return Input
 
-def mainParse(string,startFlag,Endflag,reFlags=re.S, parseType=raw):
-    rawParse = parseFlags(string,startFlag,Endflag,reFlags)
+
+def mainParse(string, startFlag, Endflag, reFlags=re.S, parseType=raw):
+    rawParse = parseFlags(string, startFlag, Endflag, reFlags)
     return parseType(rawParse)
 
-def dictParse(title,string,startFlag,Endflag,reFlags=re.S, parseType=raw):
-    rawParse = parseFlags(string,startFlag,Endflag,reFlags)
-    return {title : parseType(rawParse)}
+
+def dictParse(title, string, startFlag, Endflag, reFlags=re.S, parseType=raw):
+    rawParse = parseFlags(string, startFlag, Endflag, reFlags)
+    return {title: parseType(rawParse)}
